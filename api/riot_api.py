1fa79c54596a37904fe606aaeabcedf179eaf3d4
KCsam11 <samquibel@icloud.com>
2025-06-12 09:57:37 +0200

    (SETUP) api item

--- api/riot_api.py
+++ api/riot_api.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+from tqdm import tqdm
 
 # Fonction pour récupérer les données depuis l'API Riot
 def fetch_data_from_riot_api(api_key, region, api_endpoint):
@@ -28,7 +29,6 @@ def recupData(api_data, file_path, name_for_log):
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, 'w', encoding='utf-8') as file: # Ajout de encoding='utf-8'
             json.dump(api_data, file, indent=4, ensure_ascii=False) # Ajout de ensure_ascii=False pour les caractères spéciaux
-        print(f"Données récupérées et enregistrées dans '{file_path}'.")
     else:
         print(f"Échec de la récupération des données des {name_for_log}.")
     
@@ -58,29 +58,45 @@ if __name__ == "__main__":
         if not os.path.exists(champions_details_folder):
             os.makedirs(champions_details_folder)
         
-        for champion_id, champion_info in champions_dict.items():
+        print(f"\nTraitement des {len(champions_dict)} champions:")
+        for champion_id, champion_info in tqdm(champions_dict.items(), desc="Champions", unit="champion"):
             champion_name = champion_info['name']
             champion_detail_endpoint = f"https://ddragon.leagueoflegends.com/cdn/{lol_version}/data/{language}/champion/{champion_id}.json"
             detailed_champion_data = fetch_data_from_riot_api(api_key, region, champion_detail_endpoint)
 
             if detailed_champion_data:
-                print(detailed_champion_data)
                 if 'data' in detailed_champion_data and champion_id in detailed_champion_data['data']:
                     actual_data_to_save = detailed_champion_data['data'][champion_id]
                     file_path = os.path.join(champions_details_folder, f"{champion_id}.json")
                     recupData(actual_data_to_save, file_path, f"détails de {champion_name}")
                 else:
-                    print(f"    Structure de données inattendue pour {champion_name}. Données brutes : {str(detailed_champion_data)[:200]}...") # Affiche un aperçu
+                    tqdm.write(f"Structure de données inattendue pour {champion_name}. Données brutes sauvegardées.")
                     file_path = os.path.join(champions_details_folder, f"{champion_id}_raw.json")
                     recupData(detailed_champion_data, file_path, f"données brutes de {champion_name}")
             else:
-                print(f"    Échec de la récupération des détails pour {champion_name}.")
+                tqdm.write(f"Échec de la récupération des détails pour {champion_name}.")
     else:
         print("Échec de la récupération de la liste générale des champions. Impossible de continuer.")
 
     
     # Récupération des données des objets
-    items_endpoint = "https://ddragon.leagueoflegends.com/cdn/15.11.1/data/en_US/item.json"
-    api_data = fetch_data_from_riot_api(api_key, region, items_endpoint)
-    recupData(api_data, "items")
+    items_filename  = f'items_{language}.json'
+    items_file_path = os.path.join("data", items_filename)
+
+    items_endpoint = f"https://ddragon.leagueoflegends.com/cdn/{lol_version}/data/{language}/item.json"
+    item_data = fetch_data_from_riot_api(api_key, region, items_endpoint)
+
+    print(f"\nTraitement des items:")
+
+    with tqdm(total=1, desc="Items", unit="fichier") as pbar:
+            items_data = fetch_data_from_riot_api(api_key, region, items_endpoint)
+            pbar.update(1) # Met à jour la barre une fois l'opération terminée
+        
+    if items_data:
+        recupData(items_data, items_file_path, "items")
+        tqdm.write(f"Données des items enregistrées dans '{items_file_path}'.")
+
+    else:
+        tqdm.write("Échec de la récupération des données des items.")
+
     
